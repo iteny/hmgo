26232bfc95930519e08334b60a3b8bb3b043ad11
NicholasMars <iteny@icloud.com>
2018-04-17 20:58:48 +0800

    no message

--- validator/validator.go
+++ validator/validator.go
@@ -1,18 +1,67 @@
 package validator
 
-import "strings"
+import (
+	"strings"
+	"time"
+)
 
-// IsEmail check if the string is an email.
+const RF3339Js = "2006-01-02 15:04:05"
+const RF3339T = "2006-01-02T15:04:05"
+
+// Email check if the string is an email.
+// 检查字符串是否是电子邮件
 func Email(str string) bool {
 	// TODO uppercase letters are not supported
 	return hmEmail.MatchString(str)
 }
 
-// func Int(str string, start int, end int) bool {
-// 	if start != 0 && end != 0 {
-//
-// 	}
-// }
-func ByteLength(str string, min, max int) bool {
+// Length check if the string's length (in unicode) falls in a range.
+// Length检查字符串的长度（以unicode为单位）是否在一个范围内
+func Length(str string, min, max int) bool {
 	return strings.Count(str, "")-1 >= min && strings.Count(str, "")-1 <= max
+}
+
+// ByteLength check if the string's length (in bytes) falls in a range.
+// ByteLength检查字符串的长度（以字节为单位）是否在一个范围内
+func ByteLength(str string, min, max int) bool {
+	return len(str) >= min && len(str) <= max
+}
+
+// Null check if the string is null.
+// Null检查字符串是否为空
+func Null(str string) bool {
+	return len(str) == 0
+}
+
+// Numeric check if the string contains only numbers. Empty string is invalid.
+// Numeric检查字符串是否只包含数字。空字符串无效。
+func Numeric(str string) bool {
+	if Null(str) {
+		return false
+	}
+	return hmNumeric.MatchString(str)
+}
+
+// English check if the string contains only letters (a-zA-Z). Empty string is invalid.
+// English检查如果字符串只包含字母（a-za-z）,空字符串无效
+func English(str string) bool {
+	if Null(str) {
+		return false
+	}
+	return hmEnglish.MatchString(str)
+}
+
+// EnglishNumeric check if the string contains only letters and numbers. Empty string is invalid.
+// EnglishNumeric检查字符串是否只包含字母和数字。空字符串无效
+func EnglishNumeric(str string) bool {
+	if Null(str) {
+		return false
+	}
+	return hmEnglishNumeric.MatchString(str)
+}
+
+// IsTime check if string is valid according to given format
+func Time(str string, format string) bool {
+	_, err := time.Parse(format, str)
+	return err == nil
 }
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -52,11 +52,252 @@ func TestLength(t *testing.T) {
 		{"阿斯蒂", 0, 3},
 	}
 	for _, test := range tests {
+		result := Length(test.param, test.min, test.max)
+		if result {
+			t.Errorf("Execute Length(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute Length(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestByteLength(t *testing.T) {
+	t.Parallel()
+	var tests = []struct {
+		param string
+		min   int
+		max   int
+	}{
+		{"asdfasdf", 1, 3},
+		{"213", 1, 3},
+		{"2134", 1, 3},
+		{"", 1, 3},
+		{"asdfasdfasdf111111111234", 1, 20},
+		{"", 0, 3},
+		{"1", 0, 3},
+		{"阿斯蒂", 0, 3},
+	}
+	for _, test := range tests {
 		result := ByteLength(test.param, test.min, test.max)
 		if result {
-			t.Errorf("Execute Email(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
+			t.Errorf("Execute ByteLength(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
 		} else {
-			t.Errorf("Execute Email(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
+			t.Errorf("Execute ByteLength(%c[1;0;34m%q,min=%d,max=%d%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test.param, test.min, test.max, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestNull(t *testing.T) {
+	t.Parallel()
+	var tests = []string{
+		"",
+		"asdfasdf",
+	}
+	for _, test := range tests {
+		result := Null(test)
+		if result {
+			t.Errorf("Execute Null(%c[1;0;34m%q%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute Null(%c[1;0;34m%q%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestEnglish(t *testing.T) {
+	t.Parallel()
+	var tests = []string{
+		"\n",
+		"\r",
+		"Ⅸ",
+		"",
+		"   fooo   ",
+		"abc!!!",
+		"abc1",
+		"abc〩",
+		"abc",
+		"소주",
+		"ABC",
+		"FoObAr",
+		"소aBC",
+		"소",
+		"달기&Co.",
+		"〩Hours",
+		"\ufff0",
+		"\u0070", //UTF-8(ASCII): p
+		"\u0026", //UTF-8(ASCII): &
+		"\u0030", //UTF-8(ASCII): 0
+		"123",
+		"0123",
+		"-00123",
+		"0",
+		"-0",
+		"123.123",
+		" ",
+		".",
+		"-1¾",
+		"1¾",
+		"〥〩",
+		"모자",
+		"ix",
+		"۳۵۶۰",
+		"1--",
+		"1-1",
+		"-",
+		"--",
+		"1++",
+		"1+1",
+		"+",
+		"++",
+		"+1",
+	}
+	for _, test := range tests {
+		result := English(test)
+		if result {
+			t.Errorf("Execute English(%c[1;0;34m%#v%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute English(%c[1;0;34m%#v%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestEnglishNumeric(t *testing.T) {
+	t.Parallel()
+	var tests = []string{
+		"\n",
+		"\r",
+		"Ⅸ",
+		"",
+		"   fooo   ",
+		"abc!!!",
+		"abc1",
+		"abc〩",
+		"abc",
+		"소주",
+		"ABC",
+		"FoObAr",
+		"소aBC",
+		"소",
+		"달기&Co.",
+		"〩Hours",
+		"\ufff0",
+		"\u0070", //UTF-8(ASCII): p
+		"\u0026", //UTF-8(ASCII): &
+		"\u0030", //UTF-8(ASCII): 0
+		"123",
+		"0123",
+		"-00123",
+		"0",
+		"-0",
+		"123.123",
+		" ",
+		".",
+		"-1¾",
+		"1¾",
+		"〥〩",
+		"모자",
+		"ix",
+		"۳۵۶۰",
+		"1--",
+		"1-1",
+		"-",
+		"--",
+		"1++",
+		"1+1",
+		"+",
+		"++",
+		"+1",
+	}
+	for _, test := range tests {
+		result := EnglishNumeric(test)
+		if result {
+			t.Errorf("Execute EnglishNumeric(%c[1;0;34m%#v%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute EnglishNumeric(%c[1;0;34m%#v%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestNumeric(t *testing.T) {
+	t.Parallel()
+	var tests = []string{
+		"\n",
+		"\r",
+		"Ⅸ",
+		"",
+		"   fooo   ",
+		"abc!!!",
+		"abc1",
+		"abc〩",
+		"abc",
+		"소주",
+		"ABC",
+		"FoObAr",
+		"소aBC",
+		"소",
+		"달기&Co.",
+		"〩Hours",
+		"\ufff0",
+		"\u0070", //UTF-8(ASCII): p
+		"\u0026", //UTF-8(ASCII): &
+		"\u0030", //UTF-8(ASCII): 0
+		"123",
+		"0123",
+		"-00123",
+		"0",
+		"-0",
+		"123.123",
+		" ",
+		".",
+		"-1¾",
+		"1¾",
+		"〥〩",
+		"모자",
+		"ix",
+		"۳۵۶۰",
+		"1--",
+		"1-1",
+		"-",
+		"--",
+		"1++",
+		"1+1",
+		"+",
+		"++",
+		"+1",
+	}
+	for _, test := range tests {
+		result := Numeric(test)
+		if result {
+			t.Errorf("Execute Numeric(%c[1;0;34m%#v%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute Numeric(%c[1;0;34m%#v%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test, 0x1B, 0x1B, result, 0x1B)
+		}
+	}
+}
+func TestTime(t *testing.T) {
+	t.Parallel()
+	var tests = []struct {
+		param  string
+		format string
+	}{
+		{"2016-12-31 11:00", RF3339Js},
+		{"2016-12-31 11:00:00", RF3339Js},
+		{"2016-12-31T11:00", RF3339Js},
+		{"2016-12-31T11:00:00", RF3339Js},
+		{"2016-12-31T11:00:00Z", RF3339Js},
+		{"2016-12-31T11:00:00+01:00", RF3339Js},
+		{"2016-12-31T11:00:00-01:00", RF3339Js},
+		{"2016-12-31T11:00:00.05Z", RF3339Js},
+		{"2016-12-31T11:00:00.05-01:00", RF3339Js},
+		{"2016-12-31T11:00:00.05+01:00", RF3339Js},
+		{"2016-12-31T11:00:00", RF3339T},
+		{"2016-12-31T11:00:00Z", RF3339T},
+		{"2016-12-31T11:00:00+01:00", RF3339T},
+		{"2016-12-31T11:00:00-01:00", RF3339T},
+		{"2016-12-31T11:00:00.05Z", RF3339T},
+		{"2016-12-31T11:00:00.05-01:00", RF3339T},
+		{"2016-12-31T11:00:00.05+01:00", RF3339T},
+	}
+	for _, test := range tests {
+		result := Time(test.param, test.format)
+		if result {
+			t.Errorf("Execute Time(%c[1;0;34m%q,format=%#v%c[0m) => %c[1;0;32m%v%c[0m ", 0x1B, test.param, test.format, 0x1B, 0x1B, result, 0x1B)
+		} else {
+			t.Errorf("Execute Time(%c[1;0;34m%q,format=%#v%c[0m) => %c[1;0;31m%v%c[0m ", 0x1B, test.param, test.format, 0x1B, 0x1B, result, 0x1B)
 		}
 	}
 }
